b134605b228f80c1cfc7e465bccb4b6c9b62b47f
AlexKovaliov <lawsansana@gmail.com>
2020-11-12 21:38:02 +0300

    init commit

---
+++ src/main/ui/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            {/*hr, provider*/}
+            <>
+                {/*<Header/>*/}
+                {/*<main/>*/}
+            </>
+        </div>
+    );
+}
+
+export default App;
